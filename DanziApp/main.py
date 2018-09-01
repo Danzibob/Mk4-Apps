1ccaa46e053aaaa47ceae2338e297e6cc9e0d68c
Danny Roberts <Danzibob@gmail.com>
2018-09-01 23:43:26 +0100

    Add a GUI

--- DanziApp/main.py
+++ DanziApp/main.py
@@ -6,17 +6,22 @@ ___dependencies___ = ["app", "database"]
 ___categories___   = ["emf"]
 ___bootstrapped___ = False
 
-import http_client, ujson, wifi, time
+import http_client, ujson, wifi, time, ugfx
 from tilda import buttons
+
+# Set up URL data
 name = database_get("display-name", "UNKN")
 URL = "94.45.255.37:9099"
+params = "name=" + name + "&btn="
 
+# set up LEDs
 pin = machine.Pin("PB13", machine.Pin.OUT)
 neo = pyb.Neopix(pin)
 
+# set up buttons
+buttons.init()
 NUM_BTNS = dir(Buttons)[:10]
 
-params = "name=" + name + "&btn="
 
 def tick():
 	for btn in NUM_BTNS:
@@ -24,14 +29,22 @@ def tick():
 			sendReq(str(btn))
 
 def sendReq(btnNum):
+	ugfx.clear(ugfx.html_color(0x002e5c))
+	ugfx.text(1, 10, "You pressed " + str(btnNum), ugfx.WHITE)
 	neo.display(0x888888)
-	time.sleep(0.1)
+	time.sleep(0.2)
 	if (not wifi.nic().is_connected()):
 		return False;
-	with http_client.post('http://'+URL, urlencoded=params) as resp:
-		if resp.text == "OK": neo.display(0x008800)
-		else: neo.display(0x880000)
+	with http_client.post('http://'+URL, urlencoded=params+str(btnNum)) as resp:
+		if resp.text == "OK": 
+			neo.display(0x008800)
+			ugfx.text(1, 30, "Sent OK", ugfx.WHITE)
+		else: 
+			neo.display(0x880000)
+			ugfx.text(1, 30, "Bad Response:" + resp.text, ugfx.WHITE)
+		time.sleep(1)
 
 while 1:
 	tick()
 	time.sleep(0.1)
+	ugfx.clear(ugfx.html_color(0x002e5c))
